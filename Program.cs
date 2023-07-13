cf98efbe1f821df9b44573faf53255224af6ab14
pRaV33N-S <praveen.srinivasan1@mphasis.com>
2023-07-13 14:26:31 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecisionMaking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number");
+            int num = int.Parse(Console.ReadLine());
+            if(num == 0)
+            {
+                Console.WriteLine($"The number {num} is Zero");
+            }
+            else if (num > 0)
+            {
+                Console.WriteLine($"The number {num} is Positive Number");
+            }
+            else
+                Console.WriteLine($"The number {num} is Negative Number");
+           var result = num % 2 == 0 ? $"The number {num} is Even" : $"The number {num} is Odd";
+           Console.WriteLine(result);
+        }
+    }
+}
